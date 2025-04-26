b4be93e2f9195e37f658896116d7e62fcc3e0712
Ruben Rizzo <rubenrizzo15@gmail.com>
2025-04-26 14:48:35 +0200

    Spostati i file nella cartella doc per il deploy

--- docs/seeProducts.js
+++ docs/seeProducts.js
@@ -27,12 +27,12 @@ function changeView(id) {
         var images = ['shirt-right.png', 'shirt-back.png', 'shirt-left.png', 'shirt-front.png'];
         var currentIndex = 0;
         intervalId = setInterval(function () {
-            productImage.src = "mediaFeels/change-shown-shirt/" + images[currentIndex];
+            productImage.src = "docs/mediaFeels/change-shown-shirt/" + images[currentIndex];
             currentIndex = (currentIndex + 1) % images.length;
         }, 2000);
     } else {
         // Visualizza l'immagine corrispondente all'ID selezionato
-        productImage.src = "mediaFeels/shirt-front.png";
+        productImage.src = "docs/mediaFeels/shirt-front.png";
     }
 }
 
